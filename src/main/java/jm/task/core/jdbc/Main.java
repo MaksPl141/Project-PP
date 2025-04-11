c27df183534f4c1666f9983170252b8e4b77a90c
maksim <rv.maks@mail.ru>
2025-04-11 21:12:07 +0300

    https://github.com/MaksPl141/PP-1.git

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,8 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        UserService service = new UserServiceImpl();
 
+        service.createUsersTable();
+
+        service.saveUser("Ivan", "Ivanov", (byte) 28);
+        service.saveUser("Oleg", "Petrov", (byte) 35);
+        service.saveUser("Maxim", "Semenov", (byte) 41);
+        service.saveUser("Igor", "Severov", (byte) 20);
+
+        List<User> users = service.getAllUsers();
+        users.forEach(System.out::println);
+
+        service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/new_schemapp";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "Root771430";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка соединения", e);
+        }
+    }
 }
